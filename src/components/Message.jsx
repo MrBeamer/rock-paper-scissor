b1f05ae7fafa4fce87347351421d4b2537105a6c
michael beamer <beamermichael@gmail.com>
2021-11-10 21:32:19 +0100

    fixed to earlier message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,7 +5,10 @@ export default function Message(props) {
   const { gameResult, resetGame, gameStarted } = props;
 
   return gameStarted ? (
-    <div className="message animate__animated animate__fadeIn">
+    <div
+      className="message animate__animated animate__fadeIn"
+      hidden={gameResult === "" ? true : false}
+    >
       <h2 className="message__title ">{gameResult}</h2>
       <button onClick={resetGame} className="message__button">
         PLAY AGAIN
