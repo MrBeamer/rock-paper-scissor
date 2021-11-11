c50670f2ba1ff7861b33ee5cc5edef9cfcc508f6
michael beamer <beamermichael@gmail.com>
2021-11-11 23:32:21 +0100

    refactoring one

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,11 +3,12 @@ import "animate.css";
 
 export default function Message(props) {
   const { gameResult, resetGame } = props;
-
-  <div className="message animate__animated animate__fadeIn">
-    <h2 className="message__title ">{gameResult}</h2>
-    <button onClick={resetGame} className="message__button">
-      PLAY AGAIN
-    </button>
-  </div>;
+  return (
+    <div className="message animate__animated animate__fadeIn">
+      <h2 className="message__title ">{gameResult}</h2>
+      <button onClick={resetGame} className="message__button">
+        PLAY AGAIN
+      </button>
+    </div>
+  );
 }
