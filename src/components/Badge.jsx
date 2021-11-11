dfd2ab1b1a18a4d03c43872549c7ba616d986d13
michael beamer <beamermichael@gmail.com>
2021-11-11 23:47:48 +0100

    refactoring one

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,10 +4,10 @@ export default function Badge(props) {
   const { customClasses, badgeIcon, handleClick, id } = props;
 
   return (
-    <button onClick={handleClick} className={`badge ${customClasses}`} id={id}>
+    <div onClick={handleClick} className={`badge ${customClasses}`} id={id}>
       <div className="badge__circle">
         <img className="badge__icon" src={badgeIcon} alt="icon"></img>
       </div>
-    </button>
+    </div>
   );
 }
